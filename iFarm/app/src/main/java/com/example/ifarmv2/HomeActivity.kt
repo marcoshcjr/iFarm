aed5df00429682c47bcafcebf9cf3ea5b84cd222
marcoshcjr <51098941+marcoshcjr@users.noreply.github.com>
2020-09-17 22:51:13 -0300

    Add files via upload

--- iFarm/app/src/main/java/com/example/ifarmv2/HomeActivity.kt
+++ iFarm/app/src/main/java/com/example/ifarmv2/HomeActivity.kt
@@ -1,11 +1,29 @@
 package com.example.ifarmv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bundle: Bundle? = intent.extras
+
+        val id = bundle?.get("id")
+        val nome = bundle?.get("nome")
+
+        Toast.makeText(applicationContext, "ID: " + id.toString() + " NOME:" + nome,
+            Toast.LENGTH_LONG).show()
+
+        button4.setOnClickListener() {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- iFarm/app/src/main/java/com/example/ifarmv2/MainActivity.kt
+++ iFarm/app/src/main/java/com/example/ifarmv2/MainActivity.kt
@@ -4,24 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mudarParaActivityHome()
-    }
 
-    private fun mudarParaActivityHome() {
-        val intent = Intent(this, HomeActivity::class.java)
+        button.setOnClickListener() {
+            intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("id", 1)
+            intent.putExtra("nome", "José")
+            startActivity(intent)
+        }
 
-        Handler().postDelayed({
-            intent.change()
-        }, 3000)
-    }
 
-    fun Intent.change() {
-        startActivity(this)
-        finish()
     }
 }
